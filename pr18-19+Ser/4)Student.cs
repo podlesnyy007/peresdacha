2300fbfacb1ad6e84df1d240e4ef9c946857db1f
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2024-11-13 00:29:13 +0400

    Create 4)Student.cs

---
+++ pr18-19+Ser/4)Student.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Student : Persona
+    {
+        public int Course { get; set; }
+
+        public Student(string name, DateTime dateOfBirth, string faculty, int course)
+            : base(name, dateOfBirth, faculty)
+        {
+            Course = course;
+        }
+
+        public override int GetAge()
+        {
+            int age = DateTime.Now.Year - DateOfBirth.Year;
+            if (DateOfBirth.AddYears(age) > DateTime.Now)
+                --age;
+            return age;
+        }
+
+        public override string GetInformation()
+        {
+            return
+                $"Имя студента: {Name}\n" +
+                $"Дата рождения: {DateOfBirth.ToShortDateString()}\n" +
+                $"Возраст: {GetAge()}\n" +
+                $"Факультет: {Faculty}\n" +
+                $"Курс:{Course}";
+        }
+    }
+}
